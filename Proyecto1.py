79f0c1852da1b3cb7830222dde1d7f00f5cf0590
Juan Alegría <juanszalegria@gmail.com>
2022-03-30 18:25:03 -0500

    Perf: cost of 0

--- Proyecto1.py
+++ Proyecto1.py
@@ -21,7 +21,9 @@ def torreTp(pisActual, cuarActual, numPisos, numCuartos, numPortales, energia, p
     # Se verifica cada portal en el piso actual y los caminos que salen de este
     for portal in portales[pisActual]:
             coste = torreTp(portal[1], portal[2], numPisos, numCuartos, numPortales, energia, portales) + \
-                (abs(cuarActual - (portal[0]))) * energia[pisActual]
+                    (abs(cuarActual - (portal[0]))) * energia[pisActual]
+            if coste == 0:
+                return 0  
             if coste < minimo:  # Se retorna el coste mínimo de todos estos posibles caminos
                 minimo = coste
     return minimo
@@ -42,10 +44,10 @@ def main():
         # Se consiguen los portales y se ingresan en la lista 'portales'
         for _ in range(numPortales):
             infoPortal = input().split()
-            piso0, cuarto0, piso1, cuarto1 = tuple(int(e) for e in infoPortal)
-            if piso0 - 1 not in portales:
-                portales[piso0 - 1] = []
-            portales[piso0 - 1].append([cuarto0 - 1, piso1 - 1, cuarto1 - 1])
+            piso_i, cuarto_i, piso_f, cuarto_f = tuple(int(e) for e in infoPortal)
+            if piso_i - 1 not in portales:
+                portales[piso_i - 1] = []
+            portales[piso_i - 1].append([cuarto_i - 1, piso_f - 1, cuarto_f - 1])
         # Se llama a la función solución con los datos anteriores
         sol = torreTp(0, 0, numPisos, numCuartos, numPortales, energia, portales)
         if sol == math.inf:  # Si el coste es infinito entonces la solución no existe
