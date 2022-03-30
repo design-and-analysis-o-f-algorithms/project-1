17c66524f2eb06b4f4b3ee36ccc47d9ad12bd7d0
Camilo Morillo <c.morilloq@uniandes.edu.co>
2022-03-29 19:33:26 -0500

    Solucion recursiva implementada

--- Proyecto1.py
+++ Proyecto1.py
@@ -3,10 +3,33 @@
 # Proyecto 1
 
 import sys
+import math
 
+#funciones
+def hasPortal(pisActual, portales):
+    for i in portales:
+        if (i[0]-1)==pisActual:
+            return True
+    return False
+
+def torreTp(pisActual, cuarActual,numPisos, numCuartos, numPortales, energia, portales):
+    if pisActual==numPisos-1:
+        return (numCuartos-1-cuarActual)*energia[pisActual]
+    if not(hasPortal(pisActual, portales)):
+        return math.inf
+
+    minimo=math.inf
+    for i in portales:
+        if i[0]-1==pisActual:
+            coste=torreTp(i[2]-1, i[3]-1, numPisos, numCuartos, numPortales, energia, portales)+(abs(cuarActual-(i[1]-1)))*energia[pisActual]
+            if coste<minimo:
+                minimo=coste
+    return minimo
+
+#input y llamado de funcion solucion
 input = sys.stdin.readline
 numberCases = int(input()) #Numero de casos de prueba
-
+solutions=[]
 for c in range(numberCases):
     data = list(map(int,(input().split())))
     numPisos=data[0]
@@ -17,13 +40,12 @@ for c in range(numberCases):
     for p in range(numPortales):
         portal = list(map(int,(input().split())))
         portales.append(portal)
-    
-
-# IMPRESION
-# for i in flips:
-#     for j in i:
-#         if (j==i[len(i)-1]):
-#             print(j, end="")
-#         else:
-#             print (j, end =" ")
-#     print("")
+    sol=torreTp(0,0,numPisos, numCuartos, numPortales, energia, portales)
+    if sol==math.inf:
+        solutions.append("NO EXISTE")
+    else:
+        solutions.append(sol)
+        
+# output
+for i in solutions:
+    print(i)
